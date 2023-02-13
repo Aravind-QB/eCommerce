578ef2f0573b94213ed43acdf7429f3ec899b603
Aravind <aravindg@yeswearemad.com>
2023-02-13 21:20:48 +0530

    address updated

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from '../../entities/users/users.entity';
+import { Addresses } from 'src/entities/users/user-addresses';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Addresses])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [TypeOrmModule, UsersService],
